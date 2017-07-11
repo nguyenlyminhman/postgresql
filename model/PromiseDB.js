cf5c60e3ab2af24ef8558f1690023b41c3b78c77
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-07-11 12:23:20 +0700

    ok man

--- model/PromiseDB.js
+++ model/PromiseDB.js
@@ -1,29 +1,4 @@
-const pg = require('pg');
-
-var config = {
-    user: 'postgres', //env var: PGUSER
-    database: 'leeshop', //env var: PGDATABASE
-    password: 'sa', //env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, //env var: PGPORT
-    max: 3, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-};
-
-const pool = new pg.Pool(config);
-
-function queryDB(sqlString, arrData) {
-    return new Promise((resolve, reject) => {
-        pool.connect((err, client, done) => {
-            if (err) return reject(err);
-            client.query(sqlString, arrData, (errQuery, result) => {
-                done(errQuery);
-                if (errQuery) return reject(errQuery);
-                resolve(result);
-            });
-        });
-    });
-}
+const queryDB = require ('../dbpromise');
 
 function queryCategory(sql, arrData) {
     return queryDB(sql, arrData)
